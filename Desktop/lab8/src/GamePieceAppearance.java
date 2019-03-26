7f23c9cd5f4459dc89a43d2edbcf012a9153e866
Emily Lu <emily.lu@ou.edu>
2019-03-25 19:53:30 -0500

    Update GamePieceAppearance.java

--- Desktop/lab8/src/GamePieceAppearance.java
+++ Desktop/lab8/src/GamePieceAppearance.java
@@ -1,23 +1,20 @@
 
-import java.awt.Shape;
-
 public class GamePieceAppearance {
 
-	
-private Color color;
-private Shape shape;
+	private Color color;
+	private Shape shape;
 
-public GamePieceAppearance(Color color, Shape shape) {
-	this.color = color;
-	this.shape = shape;
-}
+	public GamePieceAppearance(Color color, Shape shape) {
+		this.color = color;
+		this.shape = shape;
+	}
 
 
-public Color getColor() {
-	return this.color;
-}
+	public Color getColor() {
+		return this.color;
+	}
 
-public Shape getShape() {
-	return this.shape;
-}
+	public Shape getShape() {
+		return this.shape;
+	}
 }
