a29257e8c00067128c915d721c4d822ce16a7c64
Emily Lu <emily.lu@ou.edu>
2019-03-25 19:52:32 -0500

    Update Driver.java

--- Desktop/lab8/src/Driver.java
+++ Desktop/lab8/src/Driver.java
@@ -1,6 +1,10 @@
-
 public class Driver {
-  public static void main(String[] args) {
-	    System.out.println(getPlayers());
-  }
+	
+    public static void main(String[] args) {
+    	BoardGame g = new BoardGame();
+    	g.addPlayer("lucy", GamePiece.YELLOW_BOOT, Location.KITCHEN);
+    	g.addPlayer("paul", GamePiece.BLUE_BOOT, Location.KITCHEN);
+
+    	System.out.println(g.getPlayerWithGamePiece(GamePiece.YELLOW_BOOT));
+    }
 }
